f600667cb64b8989bc2e5e5822d76c1b06bfb185
Mahataz Khandaker <km3657@nyu.edu>
2019-02-27 20:46:56 -0500

    remove .DS_Store

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ const routes = require('./routes/');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const morgan = require('morgan');
+
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -11,8 +12,8 @@ const flash = require('connect-flash');
 
 const app = express();
 const router = express.Router();
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
+//const React = require('react');
+//const ReactDOMServer = require('react-dom/server');
 
 // require('../config/passport')(passport); // pass passport for configuration
 
@@ -34,23 +35,25 @@ let port = 5000 || process.env.PORT;
 /* set up routes  WHAT DOES THIS DO */
 routes(router);
 // routes ======================================================================
-require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+app.use(bodyParser.urlencoded({ extended: false }));
+//require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 /** set up middlewares */
 app.use(bodyParser.json());
 //app.use('/static',express.static(path.join(__dirname,'static')))
-
 app.use('/api', router);
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser);//get info from html forms
-
+/*
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
-
+*/
+router.get('/', (req,res) => {
+	res.json({message: 'Hello, world!'});
+});
 /** start server */
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
