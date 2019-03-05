1c3167cf66a67f53b2b74537b1250795c12eb9f2
Casey <caseycorvino7@gmail.com>
2019-03-04 21:02:04 -0500

    #35 resolved

--- src/server/test/login.js
+++ src/server/test/login.js
@@ -6,6 +6,8 @@ let validator = require('../routes/login');
 
 let invalid_password_data = {name: "test", email: "valid@valid.com", password: ""};
 let invalid_email_data = {name: "test2", email: "blahblah", password: "12345678"}
+let long_password_data = {name: "test", email: "valid@email.com", password: "1234567891123456789012345678901"};
+
 describe ('Basic Login Validator Testing', function () {
     it ('should return an invalid password error', function (){
         let error_object = validator(invalid_password_data);
@@ -20,4 +22,10 @@ describe ('Basic Login Validator Testing', function () {
         error_object.should.have.property('isValid').equal(false);
         error_object.should.have.property('errors').deep.equal(expected_err);
     });
+  it ('should return a long password error', function (){
+    let error_object = validator(long_password_data);
+    let expected_err = { password: "Password must be at least 6 characters"}
+    error_object.should.have.property('isValid').equal(false);
+    error_object.should.have.property('errors').deep.equal(expected_err);
+  });
 });
