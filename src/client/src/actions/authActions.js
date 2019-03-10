330177d296a68cf341b5e917c5702d86b25610a9
Mahataz Khandaker <km3657@nyu.edu>
2019-03-10 00:31:33 -0500

    components render now

--- src/client/src/actions/authActions.js
+++ src/client/src/actions/authActions.js
@@ -15,7 +15,7 @@ import {
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post("/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({
@@ -27,7 +27,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("/login", userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
