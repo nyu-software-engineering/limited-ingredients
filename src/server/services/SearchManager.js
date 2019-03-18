c7c2d243e8e891416e104fff46de17b59c09f3bc
Mahataz Khandaker <km3657@nyu.edu>
2019-03-18 18:25:03 -0400

    recipes are being returned

--- src/server/services/SearchManager.js
+++ src/server/services/SearchManager.js
@@ -1,14 +1,36 @@
 
 var mongoose = require('mongoose');
-// require('./../models/Recipes');
+require('./../models/Recipes');
 const Recipes = mongoose.model('Recipe');
 
 function searchRecipes (req, res){
     const data = req.body;
+    let toBeReturned = ""
     Recipes.find().then(success=>{
-        console.log(success);
+        // console.log(success);
+        success[0].recipes.forEach(bob=>{
+            // console.log(bob.ingredients.split(" "));
+            // console.log(data.query);
+            if(bob.ingredients.split(" ").includes(data.query)){
+                
+                toBeReturned += bob.name + ","
+                console.log(bob.name);
+            }
+                // if (bob.ingredients[0].includes(req.body)) {
+                //     toBeReturned += bob.name + ","
+                // }
+            
+            /*
+            if(bob.ingredients.incudes(req.body)){
+                toBeReturned += bob.name + ","
+
+            }*/
+        })
     })
-    return res.send("HELLO");
+    setTimeout(() =>{
+        return res.send(toBeReturned);
+
+    }, 3000)
     // console.log("data in SearchManager: ", data);
     // return res.json({recipe: "dummy recipe"});
 }
