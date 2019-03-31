1a9a398e268403415dcdf1bf4f970caccb523cf7
Mahataz Khandaker <km3657@nyu.edu>
2019-03-31 11:16:05 -0400

    addressing task #71, task #70

--- src/server/services/SearchManager.js
+++ src/server/services/SearchManager.js
@@ -9,44 +9,35 @@ function searchRecipes (req, res){
     let toBeReturned = ""
     console.log("query: ", data);
     Recipes.find().then(success=>{
-        // console.log(success);
-        //console.log(success[0].recipes.length)
-        success[0].recipes.forEach(bob=>{
-            // console.log(bob.ingredients.split(" "));
-            /*
-            if(bob.ingredients.split(" ").includes(data.query)){
-                
-                toBeReturned += bob.name + ","
-                console.log(bob.name);
+        console.log("success length, ", success.length);
+        success.forEach (recipe => {
+            let ingredients = JSON.parse("[" + recipe.ingredients + "]");
+            //let ingredients = JSON.parse(recipe.ingredients)
+            //console.log("type of ingredients[0]: ", typeof(ingredients[0]))
+            //console.log("ingredients[0]", ingredients[0]);
+            if (ingredients[0]){
+                ingredients[0].forEach((ingredient) => {
+                    //console.log("ingredient: ", ingredient)
+                    
+                    data.forEach(search => {
+                        if (ingredient.includes(search)){
+                            toBeReturned += recipe + "\n"
+                            //console.log(recipe.name, " has the ingredient ", search);
+                        }
+                    })
+                    
+                    
+                });
             }
-            */
-            //let ingredients = bob.ingredients.split(",")
-            //let ingredients = JSON.stringify(bob.ingredients)
-            let ingredients = JSON.parse("[" + bob.ingredients + "]");
-            console.log("recipe: ")
-            console.log("type of ingredients: ", typeof(ingredients[0]))
-            //console.log("ingredients ", ingredients)
-            //ingredients = ingredients[0].split(",")
-            ingredients[0].forEach((ingredient) => {
-                //console.log("ingredient: ", ingredient)
-                if (ingredient.includes(data.query)){
-                    toBeReturned += bob.name + ","
-                    console.log(bob.name, " has the ingredient ", data.query);
-
-                }
-            })
-                // if (bob.ingredients[0].includes(req.body)) {
-                //     toBeReturned += bob.name + ","
-                // }
+            else{
+                console.log("ingredients not defined");
+            }
             
-            /*
-            if(bob.ingredients.incudes(req.body)){
-                toBeReturned += bob.name + ","
-
-            }*/
-        })
+        });
+        
     })
     setTimeout(() =>{
+        console.log("toBeReturned: ", toBeReturned)
         return res.send(toBeReturned);
 
     }, 3000)
