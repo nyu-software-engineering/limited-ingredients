d8d33a955002a4ebc13209172e26b188f959d065
Sunita Reddy <sunitareddy@10-16-208-201.dynapool.wireless.nyu.edu>
2019-04-30 15:31:14 -0400

    fixed recipe issue

--- src/client/src/components/forms/RecipeForm.js
+++ src/client/src/components/forms/RecipeForm.js
@@ -68,7 +68,7 @@ class RecipeForm extends Component {
             }
             return <a style={moreButton} className='more-button' onClick={(e)=>this.onRecipeClick(e,rec._id)}>More +</a>
         }
-    } 
+    }
 
 
     //render the recipes
@@ -77,18 +77,18 @@ class RecipeForm extends Component {
         const recipes = this.props.recipes.recipes;
         //console.log("recipes in createRecipes: ", recipes);
         //issue: reloading the page does not reflect that a saved recipe was saved
-        
+
         // (this.state.savedRecipes);
         // console.log(recipes);console.log
         return recipes.map( (rec, i) => {
-            
+
             return <div key = {i} className='recipe-container'>
                         <div className='recipe-left'>
                             <img src={rec.imageURL} />
                         </div>
                         <div className='recipe-middle'>
                             <h3>{rec.name}</h3>
-                            <p>Prep Time: {rec.prepTime} Cook Time: {rec.cookTime} Total Time: {rec.totalTime}</p>
+                            <p>Prep Time: {rec.prepTime.replace("PT", "")} Cook Time: {rec.cookTime.replace("PT", "")} Total Time: {rec.totalTimereplace("PT", "")}</p>
                             {this.renderMoreButton(rec)}
                             {this.renderSubMenu(rec)}
                         </div>
@@ -98,7 +98,7 @@ class RecipeForm extends Component {
                     </div>
 
         });
-        
+
     }
     renderHeart(recId){
         const savedRecipes = this.state.savedRecipes;
@@ -175,9 +175,9 @@ RecipeForm.propTypes = {
             errors: state.errors,
             recipes: state.recipe
         }
-      
+
   }
-    
+
 /*
 export default connect (
     mapStateToProps,
